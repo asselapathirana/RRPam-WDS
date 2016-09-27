60d251c425971755096701df23c6f5a6dd4bbab1
Assela Pathirana <assela@pathirana.net>
2016-09-27 18:47:03 +0200

    Update __main__.py

--- src/rrpam_wds/__main__.py
+++ src/rrpam_wds/__main__.py
@@ -10,6 +10,6 @@ Why does this file exist, and why __main__? For more info, read:
 """
 from rrpam_wds.cli import main
 
-# pragma: no cover
-if __name__ == "__main__":
+
+if __name__ == "__main__": # pragma: no cover
     main()
