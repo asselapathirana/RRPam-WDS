5eaa72e602cf53086559a5bc29c2183a6838b024
ak2ls2py <python2.learnsys@gmail.com>
2016-11-13 10:29:29 -0500

    now all the old tests pass. Going to develop ahead

--- src/rrpam_wds/hydraulic_services.py
+++ src/rrpam_wds/hydraulic_services.py
@@ -1,43 +1,57 @@
-import tempfile, os
+import tempfile
 from epanettools.epanettools import EPANetSimulation
 from epanettools.epanettools import Node, Link
 
-def _get_total_demand(ensim):
-    ensim.run()
-    total = 0.0
-    st = ensim.network.tsteps
-    j = Node.node_types['JUNCTION']
-    for (i, node) in [(i, x) for i, x in ensim.network.nodes.items() if x.node_type == j]:
-        d = Node.value_type["EN_DEMAND"]
-        dem = [x * y for x, y in zip(node.results[d], st)]
-        total = total + sum(dem)
-    return total
 
 
 class pdd_service(object):
 
     def __init__(self, epanet_network):
         self.open_network(epanet_network)
+        self.orig_networkfile=epanet_network
 
     def open_network(self, epanet_network):
         self.es = EPANetSimulation(epanet_network, pdd=True)
 
     def get_total_demand(self):
-        return _get_total_demand(self.es)
+        self.es.run()
+        total = 0.0
+        st = self.es.network.tsteps
+        j = Node.node_types['JUNCTION']
+        for (i, node) in [(i, x) for i, x in self.es.network.nodes.items() if x.node_type == j]:
+            d = Node.value_type["EN_DEMAND"]
+            dem = [x * y for x, y in zip(node.results[d], st)]
+            total = total + sum(dem)
+        return total    
     
+    def _c_and_r(self,vals):
+        try:
+            r=vals[0]
+            results=vals[1]
+        except: 
+            r=vals
+            results=None
+        if(r != 0):
+            raise Exception("epanettools error!")        
+        return results
 
-   
-    def get_pipe_closed_demand(self,pipeindex):
-        fd,f=tempfile.mkstemp(suffix=".inp", prefix="epanet_", dir=tempfile.gettempdir(), text=True)
+    def get_pipe_closed_demand(self, pipeindex,dia_factor):
+        import os
+        prefix_="epanet_"+self.es.ENgetlinkid(pipeindex)[1]
+        fd, f = tempfile.mkstemp(
+            suffix=".inp", prefix=prefix_, dir=tempfile.gettempdir(), text=True)
         os.close(fd)
-        d=Link.value_type['EN_DIAMETER']
-        if (pipeindex > 0):
-            ret,dsmall=self.es.ENgetlinkvalue(pipeindex,d)
-            r=self.es.ENsetlinkvalue(pipeindex,d,dsmall)
+        d = Link.value_type['EN_DIAMETER']
+
+        ret, diam = self.es.ENgetlinkvalue(pipeindex, d)
+        dsmall=diam/float(dia_factor)
+        self._c_and_r(self.es.ENsetlinkvalue(pipeindex, d, dsmall))
         self.es.ENsaveinpfile(f)
-        e2=EPANetSimulation(f)
-        return _get_total_demand(e2)
-    
+        self._c_and_r(self.es.ENsetlinkvalue(pipeindex, d, diam)) # reset diameter
+        self.es = EPANetSimulation(f,pdd=True)
+        demand=self.get_total_demand()
+        self.es=EPANetSimulation(self.orig_networkfile) # reset original network
+        return demand
 
 
 if __name__ == "__main__":  # pragma no cover
@@ -45,4 +59,6 @@ if __name__ == "__main__":  # pragma no cover
     from epanettools.examples import simple
     file = os.path.join(os.path.dirname(simple.__file__), 'Net3.inp')
     pd = pdd_service(file)
-    print(pd.get_total_demand())
+    total=pd.get_total_demand()
+    for i in range(6,120):
+        print ("%4.2f" % (pd.get_pipe_closed_demand(i,100.)/total))
--- tests/test_hydraulic_services.py
+++ tests/test_hydraulic_services.py
@@ -7,15 +7,16 @@ import rrpam_wds.examples as ex
 from rrpam_wds import hydraulic_services as hs
 
 TOTAL_DEMAND_EX1 = 95045830.
-TOTAL_DEMAND_EX2 = 955466043.
+TOTAL_DEMAND_EX2 = 945660687.
 TOTAL_DEMAND_EX3 = 965890.
+
 
 class Testhydraulicservices(unittest.TestCase):
 
     def setUp(self):
-        self.e0 = hs.pdd_service(ex.networks[0])
-        self.e1 = hs.pdd_service(ex.networks[1])
-        self.e2 = hs.pdd_service(ex.networks[2])
+        self.e1 = hs.pdd_service(ex.networks[0])
+        self.e2 = hs.pdd_service(ex.networks[1])
+        self.e3 = hs.pdd_service(ex.networks[2])
 
     def tearDown(self):
         pass
@@ -27,12 +28,12 @@ class Testhydraulicservices(unittest.TestCase):
     def test_pdd_service_get_total_demand_calls_run_in_EPANetSimulation(self):
         with patch.object(EPANetSimulation, 'run', autospec=True) as mock_run:
             self.assertFalse(mock_run.called)
-            self.e0.get_total_demand()
+            self.e1.get_total_demand()
             self.assertTrue(mock_run.called)
 
     def test_pdd_service_get_total_demand_will_not_make_initialize_in_EPANetSimulation_called(self):
         with patch.object(EPANetSimulation, 'initialize', autospec=True) as mock_run:
-            self.e0.get_total_demand()
+            self.e1.get_total_demand()
             self.assertFalse(mock_run.called)
 
     def test_pdd_service_object_creation_will_make_initialize_in_EPANetSimulation_called_with_pdd_True(
@@ -42,20 +43,54 @@ class Testhydraulicservices(unittest.TestCase):
             mock_run.assert_called_once_with(pdds.es, ex.networks[1], pdd=True)
 
     def test_total_demand_returns_correct_value_ex1(self):
-        print(self.e0.es.OriginalInputFileName)
-        self.assertAlmostEqual(self.e0.get_total_demand(), TOTAL_DEMAND_EX1, delta=TOTAL_DEMAND_EX1/100.)
+        print(self.e1.es.OriginalInputFileName)
+        self.assertAlmostEqual(
+            self.e1.get_total_demand(),
+            TOTAL_DEMAND_EX1,
+            delta=TOTAL_DEMAND_EX1 / 100.)
 
     def test_total_demand_returns_correct_value_ex2(self):
-        print(self.e1.es.OriginalInputFileName)
-        self.assertAlmostEqual(self.e1.get_total_demand(), TOTAL_DEMAND_EX2, delta=TOTAL_DEMAND_EX2/100.)
+        print(self.e2.es.OriginalInputFileName)
+        self.assertAlmostEqual(
+            self.e2.get_total_demand(),
+            TOTAL_DEMAND_EX2,
+            delta=TOTAL_DEMAND_EX2 / 100.)
 
     def test_total_demand_returns_correct_value_ex3(self):
-        print(self.e2.es.OriginalInputFileName)
-        self.assertAlmostEqual(self.e2.get_total_demand(), TOTAL_DEMAND_EX3, delta=TOTAL_DEMAND_EX3/100.)
-        
+        print(self.e3.es.OriginalInputFileName)
+        self.assertAlmostEqual(
+            self.e3.get_total_demand(),
+            TOTAL_DEMAND_EX3,
+            delta=TOTAL_DEMAND_EX3 / 100.)
+
     def test_after_closing_a_pipe_pipe_closed_demand_returns_correct_value_ex1_1(self):
-        self.assertAlmostEqual(self.e0.get_pipe_closed_demand(1),1000,delta=TOTAL_DEMAND_EX1/100.)
+        """ We try 2 links in A,B,A pattern, to make sure there are no residual errors
+        (like keeping pipe A closed when closing B). """
+        self.assertAlmostEqual(
+            self.e1.get_pipe_closed_demand(1,100)/TOTAL_DEMAND_EX1,
+            .2975,delta=.01)
+        self.assertAlmostEqual(
+            self.e1.get_pipe_closed_demand(2,100)/TOTAL_DEMAND_EX1,
+            1.0,delta=.01)        
+        self.assertAlmostEqual(
+            self.e1.get_pipe_closed_demand(1,100)/TOTAL_DEMAND_EX1,
+            0.2975,delta=.01)
+    
+    def test_after_closing_a_pipe_pipe_closed_demand_returns_correct_value_ex2_1(self):
+        """ We try 2 links in A,B,A pattern, to make sure there are no residual errors
+        (like keeping pipe A closed when closing B). """
+        self.assertAlmostEqual(
+            self.e2.get_pipe_closed_demand(115,100)/TOTAL_DEMAND_EX2,
+            .911,delta=.01)
+        self.assertAlmostEqual(
+            self.e2.get_pipe_closed_demand(51,100)/TOTAL_DEMAND_EX2,
+            .843,delta=.01)        
+        self.assertAlmostEqual(
+            self.e2.get_pipe_closed_demand(30,10)/TOTAL_DEMAND_EX2,
+            0.976,delta=.01)  
+        self.assertAlmostEqual(
+            self.e2.get_pipe_closed_demand(115,100)/TOTAL_DEMAND_EX2,
+            .911,delta=.01)        
 
-
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
     unittest.main(verbosity=2)
