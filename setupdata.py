ef107c76c5c271a2912eb786e0ef8f3b80ffe08a
Assela Pathirana <assela@pathirana.net>
2016-11-25 19:28:31 +0100

    freeze.py reads values from setup.py and writes appropriate innosetup script

---
+++ setupdata.py
@@ -0,0 +1,7 @@
+name='rrpam-wds'
+version='0.1.0'
+license='GPLV3'
+description='Risk-based renewal planning for asset management of water distribution systems'
+author='Assela Pathirana'
+author_email='assela@pathirana.net'
+url='https://github.com/asselapathirana/RRPam-WDS'
