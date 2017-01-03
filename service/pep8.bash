fb2c3cef1b51882ac6f0668a2ce937cf14ccb244
ak2ls2py <python2.learnsys@gmail.com>
2017-01-03 11:03:25 -0500

    bash script change

--- service/pep8.bash
+++ service/pep8.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e 
-isort *.py  src  service/*.py -rc
-autopep8 --in-place --recursive --max-line-length=100 src  service/*.py *.py  -a
-autoflake --remove-all-unused-imports --remove-all-unused-imports --in-place -r src service/*.py *.py 
+rootlevel='freeze.py  setupdata.py  setup.py'
+isort ${rootlevel}  src  service/*.py -rc
+autopep8 --in-place --recursive --max-line-length=100 src  service/*.py ${rootlevel} -a
+autoflake --remove-all-unused-imports --remove-all-unused-imports --in-place -r src service/*.py ${rootlevel}
 flake8 src *.py
 isort --check-only --diff --recursive src  *.py service/*.py
 check-manifest
