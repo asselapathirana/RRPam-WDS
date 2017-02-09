71159f006fd33a2269ecf1c63f16940d194c89ec
ak2ls2py <python2.learnsys@gmail.com>
2017-02-09 12:12:21 -0500

    Bump version: 0.1.0 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ project = 'RRPam-WDS'
 year = '2017'
 author = 'Assela Pathirana'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.0'
+version = release = '0.2.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
